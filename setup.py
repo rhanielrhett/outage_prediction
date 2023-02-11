53f497969899de1873df9c36bb7b619f8c387103
Rhaniel delos Santos <rrmdelossantos@gmail.com>
2023-02-11 22:39:07 +0800

    Created new repository for outage prediction

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Outage prediction for US states',
+    author='Rhaniel',
+    license='MIT',
+)
